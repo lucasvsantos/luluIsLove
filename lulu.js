aa5903fb15826f47a125d4634973ee9a2f6b1106
Lucas Victor Santos <lvictors@everis.com>
2018-10-01 08:21:41 -0300

    Código refatorado, agora funcional

--- lulu.js
+++ lulu.js
@@ -1,80 +1,77 @@
-(function() {
-	//lulu 
-	var main = function($) { 
-		
-		var self = $.lulu = new function(){};
-		
-		$.extend(self, {
-			luluImgs : [
-                './lulu.jpeg'
-			],
-			handleImages : function (lstImgs, time)
-			{
-				$.each($('img'), function(i,item) { 
-					//Skip if image is already replaced
-					if($.inArray($(item).attr('src'), lstImgs) == -1)
-					{
-						var h = $(item).height();
-						var w = $(item).width();
-						
-						//If image loaded
-						if(h > 0 && w > 0)
-						{
-							//Replace
-							$(item).css('width', w + 'px').css('height', h + 'px');
-							$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
-						}
-						else
-						{
-							//Replace when loaded
-							$(item).load(function(){
-								//Prevent 'infinite' loop
-									if($.inArray($(item).attr('src'), lstImgs) == -1)
-									{
-										var h = $(item).height();
-										var w = $(item).width();
-										$(item).css('width', w + 'px').css('height', h + 'px');
-										$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
-									}
-								});
-							}
-						}
-					});
-					
-					//Keep replacing
-					if(time > 0)
-						setTimeout(function(){self.handleImages(lstImgs, time);},time);
-				}
-			});
+//lulu 
+(function ($) {
 
-		//Run on jQuery ready
-		$(function(){
-			self.handleImages(self.luluImgs, 3000);
-		});
-	};
+    var self = $.lulu = new function () { };
 
-	//Method to load jQuery
-	function loadJS(src, callback) {
-		var s = document.createElement('script');
-		s.src = src;
-		s.async = true;
-		s.onreadystatechange = s.onload = function() {
-			var state = s.readyState;
-			if (!callback.done && (!state || /loaded|complete/.test(state))) {
-				callback.done = true;
-				callback();
-			}
-		};
-		document.getElementsByTagName('head')[0].appendChild(s);
-	}
-	
-	//Add jQuery if not present, then run main
-	if(typeof jQuery == 'undefined') {
-		loadJS(('https:' == document.location.protocol ? 'https://' : 'http://') + 'ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js', function(){
-			jQuery.noConflict();
-			main(jQuery);
-		});
-	}else {
-		main(jQuery);
-	}
- })();
+    $.extend(self, {
+
+        luluBackgrounds: [
+            'http://www.away.com.br/lulu/bg1.png'
+        ],
+
+        luluImgs: [
+            'https://i.imgur.com/xH3L2Ll.jpg'
+        ],
+
+        handleImages: function (lstImgs, time) {
+            $.each($('img'), function (i, item) {
+                //Skip if image is already replaced
+                if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                    var h = $(item).height();
+                    var w = $(item).width();
+
+                    //If image loaded
+                    if (h > 0 && w > 0) {
+                        //Replace
+                        $(item).css('width', w + 'px').css('height', h + 'px');
+                        $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                    }
+                    else {
+                        //Replace when loaded
+                        $(item).load(function () {
+                            //Prevent 'infinite' loop
+                            if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                                var h = $(item).height();
+                                var w = $(item).width();
+                                $(item).css('width', w + 'px').css('height', h + 'px');
+                                $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+                            }
+                        });
+                    }
+                }
+            });
+
+            //Keep replacing
+            if (time > 0) {
+                setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+            }
+        },
+
+        handleLogo: function (bgImgs, time) {
+            $backgroundImages = $(
+                '[class*=logo], [class*=header], [id*=header], [id*=logo],' +
+                '[class*=logo] span, [class*=header] span, [id*=header] span, [id*=logo] span,' +
+                '[class*=logo] h1, [class*=header] h1, [id*=header] h1, [id*=logo] h1,' +
+                '[class*=logo] a, [class*=header] a, [id*=header] a, [id*=logo] a'
+            )
+                .filter(function () {
+                    backgroundImg = $(this).css('background-image');
+                    return backgroundImg && backgroundImg != 'none';
+                }
+                );
+
+            $backgroundImages.each(function (i, item) {
+                $(item).css('background-image', 'url(' + bgImgs[Math.floor(Math.random() * bgImgs.length)] + ')');
+                $(item).css('background-position', '0 0');
+                $(item).css('background-repeat', 'no-repeat');
+                $(item).css('background-size', 'contain');
+            });
+        }
+    });
+
+    //Run on jQuery ready
+    $(function () {
+        self.handleImages(self.luluImgs, 3000);
+        self.handleLogo(self.luluBackgrounds, 3000);
+    });
+})(jQuery);
